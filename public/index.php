e4161533174b5ed323492c30102188f999ad0c13
winiaa <winiaaradawd14@gmail.com>
2024-03-20 00:18:51 +0700

    Initial commit and started home view

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+require __DIR__ . '/../helpers.php'; // Adjusted path to locate helpers.php correctly
+
+// Include the view file using the corrected relative path
+require __DIR__ . '/../views/home.view.php';
+
