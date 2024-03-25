9aa2d9e0f135895a187d3acf624aeb9786ddf39b
winiaa <winiaaradawd14@gmail.com>
2024-03-25 21:37:31 +0700

    I have fixed some bugs about route

--- public/index.php
+++ public/index.php
@@ -1,6 +1,26 @@
 <?php
-require __DIR__ . '/../helpers.php'; // Adjusted path to locate helpers.php correctly
+require  '../helpers.php'; // Adjusted path to locate helpers.php correctly
 
 // Include the view file using the corrected relative path
-require __DIR__ . '/../views/home.view.php';
+
+
+$routes = [
+    '/workopia/public/' => 'controllers/home.php',
+    '/workopia/public/listings' => 'controllers/listings/index.php',
+    '/workopia/public/listings/create' => 'controllers/listings/create.php',
+    '/workopia/public/404' => 'controllers/error/404.php'
+
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+
+
+
+
+if (array_key_exists($uri, $routes)) {
+    require (basePath($routes[$uri]));
+} else {
+    require basePath($routes['404']);
+}
 
